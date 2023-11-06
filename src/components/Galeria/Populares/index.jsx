3c558d910c6d79016fb0955c6de8757ccb20da0a
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-06 14:25:58 -0300

    Galeria Component and styled components created, tags.json created in the Tags folder also

---
+++ src/components/Galeria/Populares/index.jsx
@@ -0,0 +1,9 @@
+const Populares = () => {
+  return (
+    <>
+      <p>Populares</p>
+    </>
+  );
+};
+
+export default Populares;
--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import Titulo from "../Titulo";
+import Tags from "./Tags";
+import Populares from "./Populares";
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`;
+
+const SecaoFluida = styled.section`
+  flex-grow: 1;
+`;
+
 const Galeria = () => {
-  return <h2>teste</h2>;
+  return (
+    <>
+      <Tags />
+      <GaleriaContainer>
+        <SecaoFluida>
+          <Titulo>Navegue pela galeria</Titulo>
+        </SecaoFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
+  );
 };
 
 export default Galeria;
