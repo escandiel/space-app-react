f39492c9ff683be386a89f1b40e20a3626a3065a
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-06 16:39:02 -0300

    fotos.json imported to the src

--- src/components/Galeria/Tags/index.jsx
+++ src/components/Galeria/Tags/index.jsx
@@ -11,9 +11,12 @@ const TituloTag = styled.h1`
 `;
 
 const BotaoTag = styled.button`
-  border: 1px solid rgba(217, 217, 217, 0.3);
+  box-sizing: border-box;
+  border: 1px solid transparent;
   box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.4); /* Efeito de sombra */
-  padding: 10px;
+  padding: 0 18px;
+  margin: 0;
+  max-height: 49px;
   border-radius: 10px;
   border: none;
   background: rgba(217, 217, 217, 0.3);
@@ -21,12 +24,12 @@ const BotaoTag = styled.button`
   font-size: 24px;
   font-weight: 400;
   line-height: normal;
+  text-align: center;
   transition: 0.4s ease-in-out; /* Adicionando uma transição suave */
 
   &:hover {
     border: 1px solid var(--Degrad-com-rosa, #c98cf1);
     transition: 1s ease-out;
-    box-shadow: 0px 0px 6px 0px rgba(0, 0, 0, 0.8); /* Efeito de sombra */
     cursor: pointer;
   }
 `;
