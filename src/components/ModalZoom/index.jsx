c658a91457521bcbbf74adff5557937d9779b560
Victor Escandiel <joaoescandielb@gmail.com>
2023-11-07 21:57:05 -0300

    styled component Overlay created in the ModalZoom component

--- src/components/ModalZoom/index.jsx
+++ src/components/ModalZoom/index.jsx
@@ -1,12 +1,35 @@
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
 const ModalZoom = ({ foto }) => {
   return (
     <>
-      <dialog open={!!foto}>
+      {foto && (
+        <dialog open={!!foto}>
+          <Imagem foto={foto} expandida={true} />
+          <p>Exemplo do Mozilla</p>
+          <form method="dialog">
+            <button type="submit">ok</button>
+          </form>
+        </dialog>
+      )}
+
+      {/* <dialog open={!!foto}>
+        <Imagem foto={foto} expandida={true} />
         <p>Exemplo do Mozilla</p>
         <form method="dialog">
           <button type="submit">ok</button>
         </form>
-      </dialog>
+      </dialog> */}
     </>
   );
 };
